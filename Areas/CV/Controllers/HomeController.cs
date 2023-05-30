6edf05103dc2b79a142e31b45467efe2b2f8e297
Eric Faraut <eric.faraut@hotmail.fr>
2023-05-30 17:22:50 +0200

    modified:   .gitignore modified:   Areas/CV/Controllers/HomeController.cs modified:   Areas/CV/Views/Home/Index.cshtml modified:   Areas/CV/Views/Shared/Error.cshtml modified:   Areas/CV/Views/_ViewImports.cshtml modified:   Program.cs new file:   Usings.cs modified:   azure-pipelines.yml

--- Areas/CV/Controllers/HomeController.cs
+++ Areas/CV/Controllers/HomeController.cs
@@ -1,16 +1,15 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CV.Models;
-
 namespace CV.Controllers;
 [Area("CV")]
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    public HomeController(ILogger<HomeController> logger)
+    /*private readonly LoggerDummy<HomeController> _logger;
+    public HomeController(LoggerDummy<HomeController> logger)
     {
-        _logger = logger;
-    }
+        _logger = (LoggerDummy<HomeController>)logger;
+    }*/
     public IActionResult Index()
     {
         return View();
@@ -18,6 +17,10 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
+        /*if(HttpContext.Response != null)
+            _logger.LogError($"Error with the HTTP request {HttpContext.Request}. The response is: {HttpContext.Response}", HttpContext.Request, HttpContext.Response);
+        else
+            _logger.LogError($"Error with the {HttpContext.Request} request: ", HttpContext.Request, HttpContext.Response);*/
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
