5daafc557474644da6551b2d0aad26fbcf286c06
Eric Faraut <eric.faraut@hotmail.fr>
2025-01-31 16:40:25 +0100

    net8 LTS and CV front updates are done.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
-// Configure the HTTP request pipeline.
 app.UseExceptionHandler("/Home/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
